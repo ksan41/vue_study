edbefb29c4eb48ff2baa247ef0e2e9d54a57b0c8
ksan41 <aquam41@gmail.com>
2020-10-22 18:05:11 +0900

    add Components

--- src/components/TodoHeader.vue
+++ src/components/TodoHeader.vue
@@ -1,16 +1,15 @@
 <template>
   <!-- html영역 -->
-  <div>
-      header
-  </div>
+    <header>
+            <h1>Todo it!</h1>
+    </header>
 </template>
 
-<script>
-export default {
-    // js script
-}
-</script>
-
-<style>
+<style scoped>
     /* css */
+    h1 {
+        color: navy;
+        font-weight: 900;
+        margin: 2.5rem 0 1.5rem;
+    }
 </style>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,15 +1,72 @@
 <template>
   <div>
-      list
+     <ul>
+         <li v-for="(todoItem, index) in todoItems" v-bind:key="todoItem" class="shadow">
+             <i class="checkBtn fas fa-check" v-on:click="toggleComplete"></i>
+             {{todoItem}}
+             <span class="removeBtn" v-on:click="removeTodo(todoItem,index)">
+                 <i class="fas fa-trash"></i>
+             </span>
+         </li>
+     </ul>
   </div>
 </template>
 
 <script>
 export default {
+    data:function(){
+        return {
+            todoItems:[]
+        }
+    },
+    methods:{
+        removeTodo:function(todoItem,index){
+            console.log(todoItem,index);
+            localStorage.removeItem(todoItem);
+            this.todoItems.splice(index,1);
+        },
+        toggleComplete:function(){
 
+        }
+    },
+    created:function(){
+
+        // 로컬스토리지에 데이터가 있다면 li 생성
+        if(localStorage.length > 0){
+            for(var i = 0; i<localStorage.length;i++){
+                if(localStorage.key(i) !== 'loglevel:webpack-dev-server'){
+                    this.todoItems.push(localStorage.key(i));
+                }
+            }
+        }
+    }
 }
 </script>
 
-<style>
-
+<style scoped>
+ul {
+    list-style-type: none;
+    padding-left: 0px;
+    margin-top: 0;
+    text-align: left;
+}
+li{
+    display: flex;
+    min-height: 50px;
+    height: 50px;
+    line-height: 50px;
+    margin: 0.5rem 0;
+    padding: 0 0.9rem;
+    background: white;
+    border-radius: 5px;
+}
+.checkBth{
+    line-height: 45px;
+    color: #62acde;
+    margin-right: 5px;
+}
+.removeBtn{
+    margin-left: auto;
+    color: #de4343;
+}
 </style>
