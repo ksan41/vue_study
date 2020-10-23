74d0e288cf99323a0d887702368f547d7e8d9cb2
ksan41 <aquam41@gmail.com>
2020-10-23 16:31:33 +0900

    리스트 애니메이션 추가

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-     <ul>
+      <transition-group name="list" tag="ul">
          <li v-for="(todoItem, index) in propsdata" v-bind:key="todoItem.item" class="shadow">
              <i class="checkBtn fas fa-check" v-bind:class="{checkBtnCompleted: todoItem.completed}" 
                 v-on:click="toggleComplete(todoItem,index)"></i>
@@ -9,7 +9,7 @@
                  <i class="fas fa-trash"></i>
              </span>
          </li>
-     </ul>
+      </transition-group>
   </div>
 </template>
 
@@ -60,4 +60,15 @@ li{
 .checkBtnCompleted{
     color:lightgray;
 }
+
+/* 리스트 아이템 트랜지션 효과 */
+.list-enter-active, .list-leave-active {
+  transition: all 0.4s;
+}
+.list-enter, .list-leave-to /* .list-leave-active below version 2.1.8 */ {
+  opacity: 0;
+  transform: translateY(30px);
+}
+/* 리스트 아이템 트랜지션 효과 */
+
 </style>
